7b942f7627ca29adf62cf1506def250732a59c20
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-08-12 23:51:22 -0300

    Respostas ao Desafio

--- core/views.py
+++ core/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render, HttpResponse
 
 def hello(req, nome, idade):
     return HttpResponse(f'<h1>Hello {nome}, você tem {idade} anos!</h1>')
+
+
+def soma(req, num1, num2):
+    resp = num1 + num2
+    return HttpResponse(f'<h1>Soma</h1><br /><h2>Resultado da soma: {resp}</h2>')
+
+
+def subtracao(req, num1, num2):
+    resp = num1 - num2
+    return HttpResponse(f'<h1>Subtracão</h1><br /><h2>Resultado da subtracão: {resp}</h2>')
+
+
+def divisao(req, num1, num2):
+    resp = num1 / num2
+    return HttpResponse(f'<h1>Divisão</h1><br /><h2>Resultado da divisão: {resp}</h2>')
+
+
+def multiplicacao(req, num1, num2):
+    resp = num1 * num2
+    return HttpResponse(f'<h1>Multiplicacão</h1><br /><h2>Resultado da multiplicacão: {resp}</h2>')
